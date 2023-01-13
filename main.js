feee88a1002cdee97371b4f65a634a94a1fce3bd
AlexT <uaskylight@gmail.com>
2023-01-13 12:44:33 +0200

    task-11

---
+++ main.js
@@ -0,0 +1,7 @@
+function calculateTotalPrice(orderedQuantity, pricePerItem) {
+  // Change code below this line
+  const totalPrice = orderedQuantity * pricePerItem;
+
+  // Change code above this line
+  return totalPrice;
+}
