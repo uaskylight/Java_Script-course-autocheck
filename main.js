4b0f24007ac7abb390af266f2ed4002d925ce611
AlexT <uaskylight@gmail.com>
2023-01-13 12:15:36 +0200

    task-10

---
+++ main.js
@@ -0,0 +1,6 @@
+function makeMessage(name, price) {
+  // Change code below this line
+  const message = `You picked ${name}, price per item is ${price} credits`;
+  // Change code above this line
+  return message;
+}
